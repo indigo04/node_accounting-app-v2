9c10f32b61067c0b57322c3833b8bf6a5affc59d
Vladyslav Shylin <indiggoo228@gmail.com>
2024-10-28 16:08:16 +0200

    add task solution

---
+++ src/services/users.service.js
@@ -0,0 +1,48 @@
+const { createId } = require('../utils/id.servise');
+
+let users = [];
+
+const resetUsers = () => {
+  users = [];
+};
+
+const getUsers = () => {
+  return users;
+};
+
+const getUser = (id) => {
+  return users.find((elem) => elem.id === id);
+};
+const addUser = (elem) => {
+  const elemWithId = {
+    id: createId(users),
+    ...elem,
+  };
+
+  users.push(elemWithId);
+
+  return elemWithId;
+};
+const updateUser = (newElem) => {
+  const { id } = newElem;
+
+  users = users.map((elem) => (elem.id === +id ? newElem : elem));
+};
+const deleteUser = (id) => {
+  const index = users.findIndex((elem) => elem.id === id);
+
+  if (index !== -1) {
+    users.splice(index, 1);
+  }
+
+  return users;
+};
+
+module.exports = {
+  resetUsers,
+  getUsers,
+  getUser,
+  addUser,
+  updateUser,
+  deleteUser,
+};
